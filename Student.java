e20758145e3cef699ba91537bc0c343ea16d9907
ashfiyakapali <ashfiyakapali@gmail.com>
2025-04-22 14:41:10 +0530

    add this file to the main

--- Student.java
+++ Student.java
@@ -1,3 +1,16 @@
-public class Employee {
+public class Student{
+    public static void main(String[] args){
+
+        String name = "Revathi";
+        int usn = 113;
+        int fees = 80000;
+        String college = "KLETECH";
+
+     System.out.println(" -------STUDENT DETAILS--------name");
+     System.out.println("NAME:"+name);
+     System.out.println("USN:"+usn);
+     System.out.println("FEES:"+fees);
+     System.out.println("COLLEGE:"+college);
+    }
 
 }
