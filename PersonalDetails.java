e150a0ac3c9432bf5f07ccc6faa9bbd00da3dcf7
ashfiyakapali <ashfiyakapali@gmail.com>
2025-04-22 15:18:22 +0530

    save file

---
+++ PersonalDetails.java
@@ -0,0 +1,14 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        String name = "Revathi";
+        String dateOfBirth = "12-03-2005";
+        int salary = 20000;
+        int age = 20;
+
+        System.out.println("------- PERSONAL DETAILS -------");
+        System.out.println("Name: " + name);
+        System.out.println("Date of Birth: " + dateOfBirth);
+        System.out.println("Salary: " + salary);
+        System.out.println("Age: " + age);
+    }
+}
